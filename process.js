0a2dfc40a2832372b00447d0a9e323fbb3fee4a5
LuoYunXi <LuoYunXi0407@gmail.com>
2026-06-13 01:16:27 +0800

    草，忘了 §a 这些不能被处理了

--- process.js
+++ process.js
@@ -7,8 +7,11 @@ let text = fs.readFileSync(file, 'utf8');
 
 const protectedItems = [];
 
+const protectedStrictItems = [];
+
+
 /**
- * 保护特殊标记
+ * 保护但视为英文
  */
 function protect(regex) {
     text = text.replace(regex, match => {
@@ -18,6 +21,16 @@ function protect(regex) {
     });
 }
 
+/**
+ * 保护且防止被处理
+ */
+function protectStrict(regex) {
+    text = text.replace(regex, match => {
+        const id = protectedStrictItems.length;
+        protectedStrictItems.push(match);
+        return `⟦Protected${id}⟧`;
+    });
+}
 
 
 // %s
@@ -27,13 +40,13 @@ function protect(regex) {
 protect(/%\d*\$?[a-zA-Z]/g);
 
 // :tip_virtual_button_action_build_or_use:
-protect(/:[a-zA-Z0-9_-]+:/g);
+protectStrict(/:[a-zA-Z0-9_-]+:/g);
 
 
 // §a
 // §b
 // §l
-protect(/§./g);
+protectStrict(/§./g);
 
 
 /**
@@ -46,6 +59,10 @@ text = pangu.spacingText(text);
  */
 text = text.replace(/Protected(\d+)/g, (_, index) => {
     return protectedItems[Number(index)];
+});
+
+text = text.replace(/⟦Protected⟧(\d+)/g, (_, index) => {
+    return protectedStrictItems[Number(index)];
 });
 
 /**
